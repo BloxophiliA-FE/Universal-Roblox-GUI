5dab3ec500a4debd4c6e59f2f8d1b9e91f9f7e44
Blox <131676038+BloxophiliA-FE@users.noreply.github.com>
2023-06-19 11:07:59 -0400

    Update GUI.lua

--- GUI.lua
+++ GUI.lua
@@ -56,7 +56,7 @@ local Slider = MainTab:CreateSlider({
 local Toggle = MainTab:CreateToggle({
    Name = "Infinite Jump",
    CurrentValue = false,
-   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Flag = "Toggle1234", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
    Callback = function(Value)
       loadstring(game:HttpGet(('https://raw.githubusercontent.com/BloxophiliA-FE/Universal-Roblox-GUI/main/Scripts/infjump.lua'),true))()
    end,
