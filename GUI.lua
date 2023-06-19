8d8b3a49f69001c6cbfa11125f7ce77dd124cb1c
Blox <131676038+BloxophiliA-FE@users.noreply.github.com>
2023-06-18 22:46:41 -0400

    Update GUI.lua

--- GUI.lua
+++ GUI.lua
@@ -1,3 +1,5 @@
+--This is very long code, go to README file to get short version.
+
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
